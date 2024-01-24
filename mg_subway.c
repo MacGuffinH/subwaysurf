33e83a115a4e5a2f00167ea863c145bf266b633d
mg <evil356595@outlook.com>
2024-01-24 14:33:54 +0800

    1.24

--- mg_subway.c
+++ mg_subway.c
@@ -1,21 +1,37 @@
 #include "mg_subway.h"
-#define MAX 50
+
+#define MAX 10
 const int obstacle_dis = 600;
-const int hit_obs = 50;
-const int hit_ob = 25;
-const int vplus = 10;
-const int again = 500;
+const int hit_obs =100;
+const int hit_ob = 30;
+const int vplus = 10000;
+const int again = 600;
+int survive;
 int money, s, score;
 int get_m, get_s, get_sc;//刷新前得到的金币，距离和分数
-void change(char action, Character *m);
-void Draw(int x, SDL_Rect *rect);
+void change(SDL_Event event, Character *m);
+void Draw(int x, int exist);
+int ti=0;
 SDL_Window *Window;
-SDL_Surface *surface, *background, *cha, *coin, *ob_up, *ob_down, *ob_all, *ob_death = NULL;
+SDL_Surface *surface, *background, *cha,*ch_jump,*ch_down, *coin, *ob_up, *ob_down, *ob_all, *ob_death = NULL;
 SDL_Rect rech, reob, reo;
-void Draw2(int x, SDL_Rect *rect) {
-    if (x == COIN)SDL_BlitSurface(coin, NULL, surface, &reob);
+void DrawCh(Character *m){
+    switch (m->action) {
+        case RUN:SDL_BlitSurface(cha,NULL,surface,&rech);
+            break;
+        case UP:SDL_BlitSurface(ch_jump,NULL,surface,&rech);
+            break;
+            default:SDL_BlitSurface(ch_down,NULL,surface,&rech);
+            break;
+    }
 }
+void Draw2(int x, int exist) {
+    if (!exist)return;
+    if (x == COIN)SDL_BlitSurface(coin, NULL, surface, &reo);
+}
+
 void wait();
+
 int main() {
     wait();
     SDL_Event event;
@@ -27,34 +43,33 @@ int main() {
     if (event.key.keysym.sym == SDLK_p) {
         load();
         money = s = score = 0;//3的倍数位为幸存路
-        Character m = {1, 2, RUN, 1, 1};
+        Character m = {5, 2, RUN, 1, 1};
         rech.x = 200;
         rech.y = 650;
-        SDL_BlitSurface(cha,NULL,surface,&rech);
+        SDL_BlitSurface(cha, NULL, surface, &rech);
         Obstacle obstacle[4] = {0};
         Object object[4] = {0};
         srand(time(0));
         int x;
-        int survive;//随机数
         x = rand();
         survive = x % 3 + 1;
         CreateSurvive(survive, obstacle_dis, &obstacle[3], object);
         for (int j = 1; j <= 3; j++) {
             if (j == survive)continue;
             else {
-                reob.x = j * 100;
                 create(j, obstacle_dis, &obstacle[j - 1]);
             }
         }
         SDL_UpdateWindowSurface(Window);
-        //for (int i = 1; i <= 3; i++)printf("%d ", obstacle[i - 1].exist == 1 ? obstacle[i - 1].x : obstacle[3].x);
-        //printf("s:%d l:%d h:%d m:%d %d\n", survive, m.line, m.height, money, again - s);
-        printf("%d %d\n",reo.x,reo.y);
-        while (1) {
-            SDL_BlitSurface(background, NULL, surface, NULL);
+        for (int i = 1; i <= 3; i++)printf("%d ", obstacle[i - 1].exist == 1 ? obstacle[i - 1].x : obstacle[3].x);
+        printf("s:%d l:%d h:%d m:%d %d\n", survive, m.line, m.height, money, again - s);
+        //printf("%d %d\n",reo.x,reo.y);
+        while (m.life) {
+            SDL_Delay(20);
             refresh(1, obstacle, object, &m);
             s += m.v * 1;
-            if (0 && s >= again) {
+            if (s >= again) {
+                for(int i=0;i<=3;i++)obstacle[i].exist=0;
                 s = 0;
                 x = rand();
                 survive = x % 3 + 1;
@@ -64,9 +79,8 @@ int main() {
                     else create(j, obstacle_dis, &obstacle[j - 1]);
                 }
             }
-            printf("%d %d\n ",reo.x,reo.y);
-            //printf("s:%d l:%d h:%d m:%d %d\n", survive, m.line, m.height, money, again - s);
-            SDL_Delay(1000);
+            SDL_PollEvent(&event);
+            if(event.type==SDL_KEYDOWN)change(event,&m);
             SDL_UpdateWindowSurface(Window);
         }
     }
@@ -90,11 +104,16 @@ void wait() {
 }
 
 void refresh(int t, Obstacle obs[4], Object ob[4], Character *ch) {
-    SDL_BlitSurface(cha, NULL, surface, &rech);
+    if(ti)ti++;
+    if(ti==30){
+        ch->action=RUN;
+        ch->height=1;
+        ti=0;
+    }
     for (int i = 1; i <= 3; i++) {
         obs[i].distance -= t * (ch->v);
         ob[i].distance -= t * (ch->v);
-        if (obs[i].exist && obs[i].line == ch->line && obs[i].distance >= -hit_obs && obs[i].distance <= hit_obs) {
+        if (obs[i].exist && obs[i].line == ch->line && obs[i].distance <= hit_obs) {
             obs[i].exist = 0;
             printf("cool\n");
             if ((obs[i].x == UP_OB && (ch->height == 1 || ch->height == 0)) ||
@@ -111,18 +130,25 @@ void refresh(int t, Obstacle obs[4], Object ob[4], Character *ch) {
             ob[i].exist = 0;
         }
     }
+    rech.x = 100 * ch->line;
+    rech.y = 650;
     reo.y = reob.y += t * (ch->v);
-    reob.x = 100;
+    reob.x = 105;
+    reo.x = survive * 105;
+    SDL_BlitSurface(background, NULL, surface, NULL);
+    DrawCh(ch);
     for (int i = 1; i <= 3; i++) {
-        if (ob[i].exist)Draw(ob[i].x, &reob);
-        reob.x += 100;
+        if (i == survive)Draw(obs[3].x, obs[3].exist);
+        else Draw(obs[i - 1].x, obs[i - 1].exist);
+        reob.x += 105;
     }
-    Draw2(COIN, &reo);
+    Draw2(COIN, ob[3].exist);
     score += ch->v * t + get_m;
     if (ch->v + score / vplus >= MAX)ch->v = MAX;
     else ch->v += score / vplus;
     money += get_m;
     get_m = 0;
+    printf("%d",ch->height);
 }
 
 void create(int line, int distance, Obstacle *obs) {
@@ -134,22 +160,9 @@ void create(int line, int distance, Obstacle *obs) {
         obs->x = x;
         obs->distance = distance;
         obs->line = line;
+        reob.x = 105 * line;
         reob.y = 50;
-    }
-    switch (x) {
-        case UP_OB:
-            SDL_BlitSurface(ob_up, NULL, surface, &reob);
-            break;
-        case DOWN_OB:
-            SDL_BlitSurface(ob_down, NULL, surface, &reob);
-            break;
-        case ALL_OB:
-            SDL_BlitSurface(ob_all, NULL, surface, &reob);
-            break;
-        case DEATH_OB:
-            SDL_BlitSurface(ob_death, NULL, surface, &reob);
-            break;
-        default:
+        Draw(obs->x,obs->exist);
     }
 }
 
@@ -160,18 +173,19 @@ void CreateSurvive(int line, int distance, Obstacle *obs, Object object[]) {
     object[3].x = COIN;
     object[3].line = line;
     object[3].distance = distance;
+    reo.x = 105 * line;
+    reo.y = 50;
     if (x == 3) {
         object[3].height = 1;
+        SDL_BlitSurface(coin, NULL, surface, &reo);
         return;
     } else {
         obs->exist = 1;
         obs->x = x;
         obs->distance = distance;
         obs->line = line;
-        reo.x = reob.x = line * 100;
-        reo.y = reob.y = 50;
-        reob.h = 10;
-        reob.w = 10;
+        reob.x = line * 105;
+        reob.y = 50;
         if (x == UP_OB) {
             object[3].height = 2;
             SDL_BlitSurface(ob_up, NULL, surface, &reob);
@@ -186,25 +200,42 @@ void CreateSurvive(int line, int distance, Obstacle *obs, Object object[]) {
     SDL_BlitSurface(coin, NULL, surface, &reo);
 }
 
-void change(char action, Character *m) {
-    if (action == 'A' && m->line != 1)m->line--;
-    else if (action == 'D' && m->line != 3)m->line++;
-    else if (action == 'W' && m->height == 1)m->height++;
-    else if (action == 'S' && m->height == 1)m->height--;
+void change(SDL_Event event, Character *m) {
+    if (event.key.keysym.sym==SDLK_a&& m->line != 1){
+        m->line--;
+        rech.x-=105;
+    }
+    else if (event.key.keysym.sym==SDLK_d && m->line != 3){
+        m->line++;
+        rech.x-=105;
+    }
+    else if (event.key.keysym.sym==SDLK_w && m->height == 1){
+        ti=1;
+        m->height=2;
+        m->action=UP;
+    }
+    else if (event.key.keysym.sym==SDLK_s && m->height == 1){
+        ti=1;
+        m->height=0;
+        m->action=DOWN;
+    }
 }
 
 void load() {
     coin = IMG_Load("Image/8.png");
     cha = IMG_Load("Image/3.png");
+    ch_jump= IMG_Load("Image/10.jpg");
+    ch_down= IMG_Load("Image/9.jpg");
     ob_up = IMG_Load("Image/4.jpg");
     ob_down = IMG_Load("Image/6.jpg");
     ob_all = IMG_Load("Image/5.jpg");
     ob_death = IMG_Load("Image/7.png");
 }
 
-void Draw(int x, SDL_Rect *rect) {
+void Draw(int x, int exist) {
+    if (!exist)return;
     if (x == UP_OB)SDL_BlitSurface(ob_up, NULL, surface, &reob);
     if (x == DOWN_OB)SDL_BlitSurface(ob_down, NULL, surface, &reob);
     if (x == ALL_OB)SDL_BlitSurface(ob_all, NULL, surface, &reob);
-    if (x == DEATH_OB)SDL_BlitSurface(ob_all, NULL, surface, &reob);
+    if (x == DEATH_OB)SDL_BlitSurface(ob_death, NULL, surface, &reob);
 }
